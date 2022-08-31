8bc34c0f8242590e0b5be1249fbef9c532db68cd
Vitaly Borodulin <rainbowf@mail.ru>
2022-08-31 13:37:15 +0500

    Add echo function for VK-bot

--- vk_bot.py
+++ vk_bot.py
@@ -1,23 +1,29 @@
 import os
+import random
 
+import vk_api as vk
 from dotenv import load_dotenv
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.longpoll import VkLongPoll, VkEventType, Event
+from vk_api.vk_api import VkApiMethod
+
+
+def echo(event: Event, vk_api: VkApiMethod) -> None:
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000)
+    )
 
 
 def main() -> None:
     '''Start VK-bot.'''
     load_dotenv()
-    vk_session = vk_api.VkApi(token=os.getenv('VK_GROUP_TOKEN'))
+    vk_session = vk.VkApi(token=os.getenv('VK_GROUP_TOKEN'))
     longpoll = VkLongPoll(vk_session)
+    vk_api = vk_session.get_api()
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event=event, vk_api=vk_api)
 
 
 if __name__ == '__main__':
